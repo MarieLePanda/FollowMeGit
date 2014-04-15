a1db59c47a90b9eb292ae6b6287c66421e74472c
Pascal <pascalnnlacsap@gmail.com>
2014-04-15 18:12:55 +0200

    Formulaire create tâche
    Formulaire create tâche

--- fragments/shared/formulaire.php
+++ fragments/shared/formulaire.php
@@ -1,20 +1,13 @@
-<html>
-	<header>
-		<link rel="stylesheet" type="text/css" href="/FollowMeGit/res/stylePopUp.css"/>
-	</header>
-<body>
-	<?php
+<?php
 			session_start();
 			include($_SERVER['DOCUMENT_ROOT'].'/FollowMeGit/data/SqlFunction.php');
 			include ($_SERVER['DOCUMENT_ROOT'] . '/FollowMeGit/Object/User.php');
 			include ($_SERVER['DOCUMENT_ROOT'] . '/FollowMeGit/Object/Project.php');
-			include ($_SERVER['DOCUMENT_ROOT'] . '/FollowMeGit/Object/Task.php');
-			$unUser = new User($_SESSION['user_id'],null,null,null);
-			$user = SqlFunction::returnUser($_SESSION['user_id']);
-			$project = SqlFunction::returnProject($user);
-			$projectStatus = SqlFunction::priorite($project);
-	?>
+			$project = SqlFunction::returnProject($_GET['idProjet']);
+			//$projectStatus = SqlFunction::priorite($project);
+?>
 	<div id="Center">
+		<h1><?php echo $project->getName() ?></h1>
 		<form method="POST" id="general" action="formulaireTask.php" onsubmit="return valider();">
 			<br /><input type="text" id="nom_tache" name="nom_tache" placeholder="nom de tache"/><br/>
 			<SELECT name="statut" size="1">
@@ -30,5 +23,3 @@
 			<br /><input type="submit" id="sub" name="sub" onclick="" value="Create tache"/>
 		</form>
 	</div>
-</body>
-</html>
--- fragments/shared/pageProjet.php
+++ fragments/shared/pageProjet.php
@@ -2,7 +2,7 @@
 	$page_title = "Follow Me - Page projet ";
 	
 	include_once ($_SERVER['DOCUMENT_ROOT'] . '/FollowMeGit/fragments/shared/headerSite.php');
-	include_once ($_SERVER['DOCUMENT_ROOT'] . '/FollowMeGit/data/sqlFunction.php');
+	include_once ($_SERVER['DOCUMENT_ROOT'] . '/FollowMeGit/data/SqlFunction.php');
 	include_once ($_SERVER['DOCUMENT_ROOT'] . '/FollowMeGit/Object/Project.php');
 	include_once ($_SERVER['DOCUMENT_ROOT'] . '/FollowMeGit/Object/User.php');
 	include_once ($_SERVER['DOCUMENT_ROOT'] . '/FollowMeGit/Object/Task.php');
@@ -16,12 +16,14 @@
 ?>
 	<div id= "buttons">
 		<ul id="side-menu">
-			<ol><a href="#">Ajouter Collaborateur</a></ol>
-			<ol><a href="#">Suprimer Collaborateur</a></ol>
-			<ol><a href="#">Ajout une tache</a></ol>
-			<ol><a href="#">Modifier une tache</a></ol>
-			<ol><a href="#">Supprimer une tache</a></ol>
-			<ol><a href="#">Visualiser historique</a></ol>
+			<?php
+				echo("<ol><a onClick='onClickButtonAddTask(".$project->getId().");'>Ajouter Collaborateur</a></ol>");
+			?>
+			<ol><a href="#" onClick="MyWindow=window.open('formulaire.php','Supprimer Collaborateur','height=300,width=650,top=300,left=300'); return false;">Suprimer Collaborateur</a></ol>
+			<ol><a href="#" onClick="MyWindow=window.open('formulaire.php','Ajouter une tâche','height=300,width=650,top=300,left=300'); return false;">Ajout une tache</a></ol>
+			<ol><a href="#" onClick="MyWindow=window.open('formulaire.php','Modifier une tâche','height=300,width=650,top=300,left=300'); return false;">Modifier une tache</a></ol>
+			<ol><a href="#" onClick="MyWindow=window.open('formulaire.php','Supprimer une tâche','height=300,width=650,top=300,left=300'); return false;">Supprimer une tache</a></ol>
+			<ol><a href="#" onClick="MyWindow=window.open('formulaire.php','Visualiser Historique','height=300,width=650,top=300,left=300'); return false;">Visualiser historique</a></ol>
 		</ul>
 	</div>
 	<div id="center">
@@ -43,7 +45,7 @@
 			<TR>
 				<TH> <div id="entree"> <?php echo $p->getName()?> </div> </TH>
 				<?php 
-					foreach ($statuts as $s){
+					foreach ($statuts as $sq){
 				?>
 				<TD> 
 					<?php 
@@ -80,6 +82,10 @@
 <script>
 	function onClickTache(idProjet,idStatut,idPriorite,idTache){
 		setTimeout('location=(\"modificationTache.php?idProjet='+idProjet+'&idTache='+idTache+'&idPriorite='+idPriorite+'&idStatut='+idStatut+'\")');	
+		//alert("L'ID du statut est : " + idStatut + "\n " + "L'ID de la priorite est : " + idPriorite + "\n" + "L'ID de la tache est : "+ idTache);
+	}
+	function onClickButtonAddTask(idProjet){
+		setTimeout('location=(\"formulaire.php?idProjet='+idProjet+'\")');	
 		//alert("L'ID du statut est : " + idStatut + "\n " + "L'ID de la priorite est : " + idPriorite + "\n" + "L'ID de la tache est : "+ idTache);
 	}
 </script>
